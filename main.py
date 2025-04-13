c5e8bd0789b660bac02f06c40ee8da7ed29f4d1f
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2025-04-13 21:51:02 +0200

    Add environment variables

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from mlops-intro!")
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    """Load environment variables from the specified .env file."""
+    env_file = f".env.{environment}"
+    if not os.path.exists(env_file):
+        raise FileNotFoundError(f"Environment file '{env_file}' not found.")
+    load_dotenv(env_file)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
